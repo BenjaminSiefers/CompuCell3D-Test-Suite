16904c53e3d85f76b729c08f54e838ccb2a93235
BenjaminSiefers <bsiefers@iu.edu>
2018-09-30 01:17:58 -0400

    Custom output and file writing
    Accomplished:
    Custom user output path on windows
    writes all shell output to a file in /output named under its original filename
    Needs to be tested:
    Mac/Linux compatibility (edit file paths to match systems)
    add errors to Demos to see how it handles

--- batch_test_init.py
+++ batch_test_init.py
@@ -1,19 +1,35 @@
 import os
+import subprocess
+import getpass
 from sys import platform
 
 #Collects all .cc3d files                                  Path to Demos
 demos = [os.path.join(dp, file) for dp, dn, filenames in os.walk('Demos') for file in filenames if os.path.splitext(file)[1] == '.cc3d']
-
+#Checks if output director exits
+if not os.path.isdir("/output"):
+    os.makedirs("/output")
 #Check for OS
 if platform == 'linux' or platform == 'linux2':
     for path in demos:
-        os.system('C:/CompuCell3D-64bit/compucell3d.sh -i ' + path + ' -f 0 /C:"\"Users"\"ben"\"CC3DWorkspace/')
-        os.system('C:/CompuCell3D-64bit/runScript.sh -i ' + path + ' -f 0 /C:"\"Users"\"ben"\"CC3DWorkspace/')
+        #Gets filename
+        head, tail = os.path.split(path)
+        #Creates a file to send the output
+        f = open("output/" + tail[0:-5] + ".txt", "w")
+        f.writelines(subprocess.check_output('C:/CompuCell3D-64bit/runScript.sh -i ' + path + ' -f 0 /C:"\"Users"\"' + getpass.getuser() + '"\"CC3DWorkspace/'))
 elif platform == 'win32':
     for path in demos:
-        os.system('C:/CompuCell3D-64bit/compucell3d.bat -i ' + path + ' -f 0 -o /C:"\"Users"\"ben"\"CC3DWorkspace/')
-        os.system('C:/CompuCell3D-64bit/runScript.bat -i ' + path + ' -f 0 -o /C:"\"Users"\"ben"\"CC3DWorkspace/')
+        #Gets filename
+        head, tail = os.path.split(path)
+        #Creates a file to send the output
+        f = open("output/" + tail[0:-5] + ".txt", "w")
+        f.writelines(subprocess.check_output('C:/CompuCell3D-64bit/runScript.bat -i ' + path + ' -f 0 -o /C:"\"Users"\"' + getpass.getuser() + '"\"CC3DWorkspace/'))
+        f.close()
 elif platform == 'darwin':
     for path in demos:
-        os.system('C:/CompuCell3D-64bit/compucell3d.command -i ' + path + ' -f 0 /C:"\"Users"\"ben"\"CC3DWorkspace/')
-        os.system('C:/CompuCell3D-64bit/runScript.command -i ' + path + ' -f 0 /C:"\"Users"\"ben"\"CC3DWorkspace/')
+        #Gets filename
+        head, tail = os.path.split(path)
+        #Creates a file to send the output
+        f = open("output/" + tail[0:-5] + ".txt", "w")
+        f.writelines(subprocess.check_output('C:/CompuCell3D-64bit/runScript.command -i ' + path + ' -f 0 /C:"\"Users"\"' + getpass.getuser() + '"\"CC3DWorkspace/'))
+        f.close()
+		
