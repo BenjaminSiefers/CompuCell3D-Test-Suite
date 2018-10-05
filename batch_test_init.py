b38d0774b776ef851830facce81b1003ada1bcc8
BenjaminSiefers <bsiefers@iu.edu>
2018-10-04 23:40:31 -0400

    Error Handling and Cross-Platform Compatibility
    Changes:
    Edits files paths for users(uses standard C: for windows)
    Linux compatibility
    Collects in-depth errors
    
    Should be done:
    More through error tests
    Mac testing

--- batch_test_init.py
+++ batch_test_init.py
@@ -1,35 +1,56 @@
 import os
 import subprocess
-import getpass
 from sys import platform
 
-#Collects all .cc3d files                                  Path to Demos
+#Collects all .cc3d files                                      Path to Demos
 demos = [os.path.join(dp, file) for dp, dn, filenames in os.walk('Demos') for file in filenames if os.path.splitext(file)[1] == '.cc3d']
 #Checks if output director exits
-if not os.path.isdir("/output"):
-    os.makedirs("/output")
+output = os.path.expanduser('~') + "/CC3DWorkspace/Shell_Output/"
+
 #Check for OS
 if platform == 'linux' or platform == 'linux2':
+    if not os.path.isdir(output):
+        os.makedirs(output)
     for path in demos:
         #Gets filename
         head, tail = os.path.split(path)
         #Creates a file to send the output
-        f = open("output/" + tail[0:-5] + ".txt", "w")
-        f.writelines(subprocess.check_output('C:/CompuCell3D-64bit/runScript.sh -i ' + path + ' -f 0 /C:"\"Users"\"' + getpass.getuser() + '"\"CC3DWorkspace/'))
+        command = ["./runScript.sh", "-i", path]
+        f = open(output + tail[0:-5] + ".txt", "w")
+        try:
+            f = open(output + tail[0:-5] + ".txt", "w")
+            f.writelines(subprocess.check_output(command))
+        except subprocess.CalledProcessError as e:
+            print "error code", e.returncode, e.output
+            f.writelines("error code" + "\n" +  str(e.returncode) + "\n" + str(e.output))
+        f.close()
+
 elif platform == 'win32':
+    if not os.path.isdir("C:/CC3DWorkspace/Shell_Output"):
+        os.makedirs("C:/CC3DWorkspace/Shell_Output")
     for path in demos:
         #Gets filename
         head, tail = os.path.split(path)
         #Creates a file to send the output
-        f = open("output/" + tail[0:-5] + ".txt", "w")
-        f.writelines(subprocess.check_output('C:/CompuCell3D-64bit/runScript.bat -i ' + path + ' -f 0 -o /C:"\"Users"\"' + getpass.getuser() + '"\"CC3DWorkspace/'))
+        f = open(output + tail[0:-5] + ".txt", "w")
+        try:
+            f.writelines(subprocess.check_output("C:/CompuCell3D-64bit/runScript.bat -i " + path))
+        except subprocess.CalledProcessError as e:
+            print "error code", e.returncode, e.output
+            f.writelines("error code" + "\n" + str(e.returncode) + "\n" + e.output)
         f.close()
+
 elif platform == 'darwin':
+    if not os.path.isdir(output):
+        os.makedirs(output)
     for path in demos:
         #Gets filename
         head, tail = os.path.split(path)
         #Creates a file to send the output
-        f = open("output/" + tail[0:-5] + ".txt", "w")
-        f.writelines(subprocess.check_output('C:/CompuCell3D-64bit/runScript.command -i ' + path + ' -f 0 /C:"\"Users"\"' + getpass.getuser() + '"\"CC3DWorkspace/'))
+        f = open(output + tail[0:-5] + ".txt", "w")
+        try:
+            f.writelines(subprocess.check_output("./runScript.command -i " + path))
+        except subprocess.CalledProcessError as e:
+            print "error code", e.returncode, e.output
+            f.writelines("error code" + "\n" + str(e.returncode) + "\n" + e.output)
         f.close()
-		
