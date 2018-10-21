27b6d69908c86696d0e43c89b742be9456995526
Benjamin Siefers <bsiefers@iu.edu>
2018-10-21 19:46:47 -0400

    Minor bug fixes

--- batch_test_init.py
+++ batch_test_init.py
@@ -11,22 +11,24 @@ The default location for your  test output should be found in a folder in this d
 """
 import os
 import time
-from sys import platform
+import platform
 import sys
 time = time.strftime("%Y-%m-%d_%H-%M")
-testFile = open("TestResult/" + "TestResult" + time + ".txt", 'w')
+testFile = open("TestResult/" + "TestResult" + time + ".txt", "w")
 testFile.close()
-print("Proccessing...")
+
+print("Proccessing..." + "   This could take a VERY long time...")
 #Sends the Command to Shell	and writes a clean output filels
 def CallInShell(arguments):
     command = ""
-    for arg in arguments:
-        command += arg + " "
-    if platform != 'win32':
+    for _arg in arguments:
+        command += _arg + " "
+    if platform != "win32":
         f = open(output + tail[0:-5]  + ".txt", "w")
         FileLocation = output
         try:
-            f.writelines(os.popen(command, 'r'))
+            print(command)
+            f.writelines(os.popen(command, "r"))
         except os.error as e:
             print("#Error code:", e.returncode, e.output)
             f.writelines("Error Code:" + "\n" + str(e.returncode) + "\n" + e.output)
@@ -35,14 +37,14 @@ def CallInShell(arguments):
         f = open(FileLocation + tail[0:-5] + ".txt", "w")
         try:
             print(command)
-            f.writelines(os.popen(command, 'r'))
+            f.writelines(os.popen(command, "r"))
         except os.error as e:
             print("#Error code:", e.returncode, e.output)
             f.writelines("#Error Code:" + "\n" + str(e.returncode) + "\n" + e.output)
     noError = False
     f.close()
     #reopens it to give it a date and clean the output
-    with open(FileLocation + tail[0:-5] + ".txt") as infile, open(FileLocation + tail[0:-5] +  "temp" + '.txt', 'w') as outfile:
+    with open(FileLocation + tail[0:-5] + ".txt") as infile, open(FileLocation + tail[0:-5] +  "temp" + ".txt", "w") as outfile:
         buffer = []
         PastLine = False
         for line in infile:
@@ -52,19 +54,20 @@ def CallInShell(arguments):
             Strip = line.strip()
             if Strip == "------------------PERFORMANCE REPORT:----------------------" or Strip == "#Error code:" or PastLine:
                 PastLine = True
-                outfile.write(''.join(buffer))
+                outfile.write("".join(buffer))
                 buffer = []
                 outfile.write(line)
-    if(PastLine):
-        tempFileLocation = os.path.join(FileLocation + tail[0:-5] +  "temp" + '.txt')
+    if(not PastLine):
+        tempFileLocation = os.path.join(FileLocation + tail[0:-5] +  "temp" + ".txt")
     else:
-        tempFileLocation = os.path.join(FileLocation + tail[0:-5] +  "temp" + '.txt')
-
+        tempFileLocation = os.path.join(FileLocation + tail[0:-5] +  "temp" + ".txt")
+    
     with open("Demos/Testing/TestResult/" + "TestResult" + time + ".txt", "a") as TestResult:
-        with open(tempFileLocation, 'r') as tempFile:
+        with open(tempFileLocation, "r") as tempFile:
+            TestResult.write(platform.system() + " version: " + platform.version() + "  " +  platform.machine())
             for line in tempFile:
                 TestResult.write(line)
-
+    TestResult.close()
 #Creates a command to send the CallinShell function
 def CreateCommand():
     if(player):
@@ -80,13 +83,13 @@ def CreateCommand():
     if(customFrequency):
         arguments.append("-f")
         arguments.append(frequency)
-    for arg in args:
-        arguments.append(arg)   
+    for _arg in args:
+        arguments.append(_arg)   
     CallInShell(arguments)
 
 
 
-os.chdir('../../')
+os.chdir("../../")
 if not os.path.isdir("Demos/Testing"): os.makedirs("Demos/Testing")
 #defaults to not use the player
 player = False
@@ -97,10 +100,10 @@ customCoreName = False
 #assign indicated args to a list
 args = sys.argv
 del args[0]
-InputDirectory = 'Demos'
+InputDirectory = "Demos"
 #help command args for this file
-for arg in args:
-    if arg == "-h" or arg == "--help":
+for _arg in args:
+    if _arg == "-h" or _arg == "--help":
         #TODO write a -testOutput handler
         print("""
         -h shows a list of commands
@@ -116,59 +119,59 @@ for arg in args:
         """)
         exit()
     #takes a directory as opposed to a file location
-    elif arg == "-i":
+    elif _arg == "-i":
         InputDirectory = args[args.index("-i") + 1]
         #delete their locations for convenience later
         del args[args.index("-i") + 1]
         del args[args.index("-i")]
-    elif arg == "-testOutput":
+    elif _arg == "-testOutput":
         testOutputDirectory = args[args.index("-testOutput") + 1]
         #delete their locations for convenience later
         del args[args.index("-testOutput") + 1]
         del args[args.index("-testOutput")]
     #signals that the user wants to use the player and removes any --exitWhenDone commands
-    elif arg == "--player":
+    elif _arg == "--player":
         player = True
         del args[args.index("--player")]
         if "--exitWhenDone" in args:
             del args[args.index("--exitWhenDone")]
-    elif arg == "-f":
+    elif _arg == "-f":
         customFrequency = True
         frequency = args[args.index("-f") + 1]
         del args[args.index("-f") + 1]
         del args[args.index("-f")]
-    elif arg == "-c":
+    elif _arg == "-c":
         customCoreName = True
         coreName = args[args.index("-c") + 1]
         del args[args.index("-c") + 1]
         del args[args.index("-c")]
-    elif arg == "--noOutput":
+    elif _arg == "--noOutput":
         pass
     else:
-        print("Error: " + arg + " is an unreconized argument!")
+        print("Error: " + _arg + " is an unreconized argument!")
         print("Use the --help command to see a list of commands")
 
 #Collects all .cc3d files                                      
-demos = [os.path.join(dp, file) for dp, dn, filenames in os.walk(InputDirectory) for file in filenames if os.path.splitext(file)[1] == '.cc3d']
+demos = [os.path.join(dp, file) for dp, dn, filenames in os.walk(InputDirectory) for file in filenames if os.path.splitext(file)[1] == ".cc3d"]
 #Checks if output director exits
 output = "Demos/Testing/TestResult/"
 #Check for OS
-if platform == 'linux' or platform == 'linux2':
-    for path in demos:
+if platform.system() == "Linux":
+    for _path in demos:
         #Gets filename
         head, tail = os.path.split(path)
         ext = ".sh"
         setup = "./"
         CreateCommand()
-elif platform == 'win32':
-    for path in demos:
+elif platform.system() == "Windows":
+    for _path in demos:
         #Gets filename
         head, tail = os.path.split(path)
         ext = ".bat"
         setup = "C:/CompuCell3D-64bit/"
         CreateCommand()
-else:
-    for path in demos:
+elif platform.system() == "Darwin":
+    for _path in demos:
         #Gets filename
         head, tail = os.path.split(path)
         ext = ".command"
