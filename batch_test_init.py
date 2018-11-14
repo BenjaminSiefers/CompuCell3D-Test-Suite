d7c41e9d38d3fcce5286405dbb7382ec4962930b
Benjamin Siefers <bsiefers@iu.edu>
2018-11-14 02:24:33 -0500

    Update batch_test_init.py

--- batch_test_init.py
+++ batch_test_init.py
@@ -8,162 +8,190 @@ You can use the command line args "-o  [Path To Directory]" for directing output
 "-o" and "-i" are expected to be given directories
 For "-o" the program will send your test result as well as your normal CompuCell3D data to that directory.
 The default location for your  test output should be found in a folder in this directory.
+
+Ex.
+python batch_test_init.py -i [Input Directory] -o [Output Directory]
+
+Uses Demos directory
+python batch_test_init.py
 """
 import os
 import time
 import platform
 import sys
-#get the current time
+import shutil
+
+#get the current time DD-MM-YYYY_H-MM
 timeStart = time.strftime("%d-%m-%Y_%H-%M")
 #default output directory for test results
 testOutput = "Demos/Testing/"
 #keeps track which demo the program is on
-demoNumber = 1;
+demoNumber = 0
 
-
+#Int -> Void
+#appends the return code of the processed demo
 def appendReturnCode(returnCode):
-    file = open(testOutput + "ProcessedDemos" + timeStart + ".txt", "r")
-    contents = "";
-    i = 0
+    f = open(testOutput + "ProcessedDemos" + timeStart + ".txt", "r")
+    contents = ""
     # looks for the current demo and inserts the return value
-    for line in file:
-        contents += line;
-        if (line[:-1] == path):
+    for line in f:
+        contents += line
+        if line[:-1] == path:
             contents += "Return Code: " + str(returnCode) + "\n"
-        i += 1;
-    file.close()
-    file = open(testOutput + "ProcessedDemos" + timeStart + ".txt", "w")
-    file.writelines(contents);
-    file.close()
+    f.close()
 
-print("Proccessing...   This could take a VERY long time...")
-#Sends the Command to Shell	and writes a clean output filels
+    f = open(testOutput + "ProcessedDemos" + timeStart + ".txt", "w")
+    f.writelines(contents)
+    f.close()
+
+
+print("Processing...  This could take a VERY long time...")
+
+#List[String] -> Void
+#Sends the command to the shell stores the output depending on the outcome of the demo
 def callInShell(arguments):
     command = ""
-
+    #adds a space for each command
     for _arg in arguments:
         command += _arg + " "
-    if platform != "win32":
-        try:
-            commandOutput = os.popen(command, "r")
-            f = open(testOutput + tail[0:-5] + ".txt", "w")
-            f.writelines(commandOutput)
-            f.close()
-        except os.error as e:
-            print("#Error code:", e.returncode, e.output)
-            f = open(testOutput + tail[0:-5] + ".txt", "w")
-            f.writelines("Error Code:" + "\n" + str(e.returncode) + "\n" + e.output)
-            f.close()
-    else:
-        try:
-            commandOutput = os.popen(command, "r")
-            f = open(testOutput + tail[0:-5] + ".txt", "w")
-            f.writelines(commandOutput)
-            f.close()
-        except os.error as e:
-            print("#Error code:", e.returncode, e.output)
-            f = open(testOutput + tail[0:-5] + ".txt", "w")
-            f.writelines("#Error Code:" + "\n" + str(e.returncode) + "\n" + e.output)
-            f.close()
+    commandOutput = []
+    try:
+        for line in os.popen(command, "r"):
+            commandOutput.append(line)
+    except os.error as e:
+        print("#Error code:", e.returncode, e.output)
+        f = open(testOutput + tail[0:-5] + ".txt", "w")
+        f.writelines("Error Code:" + "\n" + str(e.returncode) + "\n" + e.output)
+        f.close()
 
-    #reopens it to give it a date and clean the output
-    with open(testOutput + tail[0:-5] + ".txt", "r") as infile, open(testOutput + tail[0:-5] + "temp" + ".txt", "w") as outfile:
-        buffer = []
-        pastLine = False
-        outfile.write(path + "\n")
-        for line in infile:
-            Strip = line.strip()
-            if Strip == "------------------PERFORMANCE REPORT:----------------------" or pastLine:
-                pastLine = True
-                outfile.write("".join(buffer))
-                buffer = []
-                outfile.write(line)
-    infile.close()
-    outfile.close()
-    if(not pastLine):
-        fileName = testOutput + tail[0:-5] + ".txt"
-    else:
-        fileName = testOutput + tail[0:-5] + "temp" + ".txt"
-    if(pastLine):
+    numberOfLinesInOutput = 0
+    #creates a list that stores a performance report if there is one
+    demoOutputToList = []
+    demoOutput = []
+    hasPerformanceReport = False
+    #adds a path to it as an identifier
+    demoOutputToList.append("SUCCESSFUL DEMO OUTPUT[DEMO #" + str(demoNumber) + "]: " + path + "\n")
+    #doesn't add lines until it encounters a performance report
+    for line in commandOutput:
+        numberOfLinesInOutput += 1
+        if line[:-1] == "------------------PERFORMANCE REPORT:----------------------" or hasPerformanceReport:
+            hasPerformanceReport = True
+            demoOutputToList.append(line)
+    # if the demo had a performance report
+    if hasPerformanceReport:
         appendReturnCode(0)
-        with open(testOutput + "SuccessfulResults" + timeStart + ".txt", "a") as testResult:
-            with open(fileName) as file:
-                for line in file:
-                    testResult.write(line)
-                testResult.write("\n\n")
-        testResult.close()
-        file.close()
+        #writes the performance report
+        with open(testOutput + "SuccessfulResults" + timeStart + ".txt", "a") as f:
+            for line in demoOutputToList:
+                f.write(line)
+            f.write("\n\n")
+        f.close()
+
+    #if the demo did not have a performance report
     else:
         appendReturnCode(1)
         if not os.path.exists(testOutput + "UnexpectedResults" + timeStart + ".txt"):
-            file = open(testOutput + "UnexpectedResults" + timeStart + ".txt", "w")
-            file.write("These are demos that have given an error or have ran to completion and did not provide a performance report.\n")
-            file.close()
-        with open(testOutput + "UnexpectedResults" + timeStart + ".txt", "a") as testResult:
-            with open(fileName) as file:
-                testResult.write("UNEXPECTED OUTPUT[DEMO #" + str(demoNumber) + "]: " + path + "\n")
-                testResult.write("OUTPUT: \n")
-                numberOfLinesInFile = 0
-                linesToBeRemoved = 0
-                contents = ""
-                for line in file:
-                    numberOfLinesInFile += 1
-                    contents += line + "\n"
-                    pass
-                for line in contents.splitlines():
-                    linesToBeRemoved+=1
-                    if numberOfLinesInFile-20 <= linesToBeRemoved:
-                        testResult.write(line)
-                    pass
-                file.close()
-                testResult.write("\n\n")
-                testResult.close()
-    os.remove(testOutput + tail[0:-5] + ".txt")
-    os.remove(testOutput + tail[0:-5] + "temp" + ".txt")
+            f = open(testOutput + "UnexpectedResults" + timeStart + ".txt", "w")
 
+            f.write(platform.system() + " version: " + platform.version() + "  " + platform.machine() + "\n")
+            if os.path.isfile("../../../CompuCell3D-64bit website.url"):
+                f.write("CompuCell3D-64bit\n")
+            elif os.path.isfile("../../../CompuCell3D-32bit website.url"):
+                f.write("CompuCell3D-32bit\n")
+            f.write("These are demos that have given an error or have ran to completion and did not provide a performance report.\n")
+            f.close()
+
+        #opens the UnexpectedResults file and writes the last 20 lines of the output from the demo
+        with open(testOutput + "UnexpectedResults" + timeStart + ".txt", "a") as testResult:
+            #tags location in UnexpectedResults with an identifier
+            testResult.write("--------------------------------------------------------------------------------------------------\n")
+            testResult.write("UNEXPECTED OUTPUT[DEMO #" + str(demoNumber) + "]: " + path + "\n")
+            testResult.write("--------------------------------------------------------------------------------------------------\n")
+            testResult.write("OUTPUT: \n")
+            #writes last 20 lines of the demo file
+            linesToBeRemoved = numberOfLinesInOutput - 20
+            for line in commandOutput:
+                if linesToBeRemoved <= 0:
+                    testResult.write(line)
+                else:
+                    linesToBeRemoved -= 1
+            testResult.write("\n\n")
+            testResult.close()
+
+    #if the user specifies no output ensure that if CC3D creates output to delete it
+    if noOutput and os.path.isdir(outputDirectory + "/" + tail[0:-5] + timeStart):
+        shutil.rmtree(outputDirectory + "/" + tail[0:-5] + timeStart)
+
+
+#Void -> Void
 #Creates a command to send the CallinShell function
 def createCommand():
-    if(player):
+    #checks if the player is going to be used
+    if player:
         arguments = [setup + "compucell3d" + ext, "--exitWhenDone"]
     else:
         arguments = [setup + "runScript" + ext]
-
+    #Takes the Demos directory by default unless specified with -i
     arguments.append("-i")
     arguments.append(path)
-    if(customOutput):
-        arguments.append("-o")
-        if(not os.path.isdir(outputDirectory + "/" + tail[0:-5] + timeStart)):
-            os.makedirs(outputDirectory + "/" + tail[0:-5] + timeStart)
-        arguments.append(outputDirectory + "/" + tail[0:-5] + timeStart)
-    if(noOutput):
+    #Sends output to CC3DWorkspace by default unless specified with -o
+    arguments.append("-o")
+    #Check the output directory exists if not create it
+    if not os.path.isdir(outputDirectory + "/" + tail[0:-5] + timeStart):
+        os.makedirs(outputDirectory + "/" + tail[0:-5] + timeStart)
+    arguments.append(outputDirectory + "/" + tail[0:-5] + timeStart)
+
+    #deletes output from CC3D
+    if noOutput:
         arguments.append("--noOutput")
+
+    #creates the arguments to be called
     for arg in args:
         arguments.append(arg)
 
     callInShell(arguments)
+#String -> Void
+
+def createNewInstance(commandForInstance):
+    os.system(commandForInstance)
+
+# String -> Void
+# writes the used to demos to a file
+def storeProcessedDemos(demoPath):
+    with open(testOutput + "ProcessedDemos" + timeStart + ".txt", 'a') as processedDemos:
+        processedDemos.writelines(demoPath + "\n")
+    processedDemos.close()
 
 
+# String -> Boolean
+# checks if a path has been processed yet
+# TODO: this was mean't for multi-instancing
+def notInProcessedDemos(demoPath):
+    with open(testOutput + "ProcessedDemos" + timeStart + ".txt", 'r') as processedDemos:
+        for line in processedDemos:
+            if demoPath + "\n" == line:
+                return False
+        return True
 
 
-
-#defaults to not use the player
+#defaults
 player = False
-uniqueOuput = False
-customScreenshot = False
-customFrequency = False
-customCoreName = False
 customOutput = False
 noOutput = False
 #assign indicated args to a list
 args = sys.argv
 del args[0]
 inputDirectory = "Demos"
-
+outputDirectory = "../CC3DWorkspace"
+#allows the intitial instance run through
+if "__batchTestStartTime__" in args:
+    time.sleep(2)
 #help command args for this file
-j = 0
-while j < len(args):
-    if (args.count("-h") == 1 or args.count("--help") == 1) and (args[j] == "-h" or arg == "--help"):
-        #TODO write a -testOutput handler
+i = 0
+#goes through the given arguments
+while i < len(args):
+    #outputs help menu
+    if "-h" in args or "--help" in args:
         print("""
 This script tests all the .cc3d scripts in a directory and collects output
 and determines whether the .cc3d files failed or ran to completion. See the 
@@ -174,92 +202,97 @@ documentation located in this directory for more details.
         -testOutput [Path to Directory] Specifies where the test results file will be located
         --noOutput Instructs CC3D to not store any project output, but tests will be saved
         --player Uses the player if you want to have screenshots
-                
+Ex.
+    python batch_test_init.py -i ../../Demos -o ../../../CC3DWorkspace --noOutput  
+    python batch_test.init.py          
         """)
         exit(0)
+    #creates the given number of instances to run of this program
+    elif "-instances" in args:
+        instances = args[args.index("-instances") + 1]
+        del args[args.index("-instances") + 1]
+        del args[args.index("-instances")]
+        userGivenCommands = "";
+        for arg in args:
+            userGivenCommands += arg + " "
+        for instance in instances:
+            createNewInstance(userGivenCommands)
+    #created for multi-threading to give new threads the correct start time
+    elif "__batchTestStartTime__" in args:
+        timeStart = args[args.index("__batchTestStartTime__") + 1]
+        del args[args.index("__batchTestStartTime__") + 1]
+        del args[args.index("__batchTestStartTime__")]
     #takes a directory as opposed to a file location
-    elif args.count("-i") == 1 and args[j] == "-i":
+    elif args.count("-i") == 1 and args[i] == "-i":
         inputDirectory = os.path.abspath(args[args.index("-i") + 1])
         #delete their locations for convenience later
         del args[args.index("-i") + 1]
         del args[args.index("-i")]
-
-    elif args.count("-o") == 1 and args[j] == "-o":
+    #sets a custom output for Demo data
+    elif args.count("-o") == 1 and args[i] == "-o":
         customOutput = True
         outputDirectory = os.path.abspath(args[args.index("-o") + 1])
         #delete their locations for convenience later
         del args[args.index("-o") + 1]
         del args[args.index("-o")]
-
-    elif args.count("-testOutput") == 1 and args[j] == "-testOutput":
+    #sets a custom output for test data
+    elif args.count("-testOutput") == 1 and args[i] == "-testOutput":
         testOutput = os.path.abspath(args[args.index("-testOutput") + 1]) + "/"
         #delete their locations for convenience later
         del args[args.index("-testOutput") + 1]
         del args[args.index("-testOutput")]
 
     #signals that the user wants to use the player and removes any --exitWhenDone commands
-    elif args.count("--player") == 1 and args[j] == "--player":
+    elif args.count("--player") == 1 and args[i] == "--player":
         player = True
         del args[args.index("--player")]
+        #makes sure --exitWhenDone isn't in the arguments they gave
         if "--exitWhenDone" in args:
             del args[args.index("--exitWhenDone")]
 
     #sets up the tests not to send any output to the CC3DWorkspace
-    elif args.count("--noOutput") == 1 and args[j] == "--noOutput":
+    elif args.count("--noOutput") == 1 and args[i] == "--noOutput":
         noOutput = True
         del args[args.index("--noOutput")]
-
+    #throw an error if the arg isn't found
     else:
-        print("Error: near " + args[j] + " is an invalid command or sequence!")
+        print("Error: near " + args[i] + " is an invalid command or sequence!")
         print("Use the --help command to see a list of commands")
         exit(1)
 
 #changes the working directory to the top directory
 os.chdir("../../")
 
-file = open(testOutput + "SuccessfulResults" + timeStart + ".txt", "w")
-file.write(platform.system() + " version: " + platform.version() + "  " + platform.machine() + "\n")
-if(os.path.isfile("../../../CompuCell3D-64bit website.url")):
-    file.write("CompuCell3D-64bit\n")
-elif(os.path.isfile("../../../CompuCell3D-32bit website.url")):
-    file.write("CompuCell3D-32bit\n")
-file.close()
-
+#stores system information in SuccessfulResults
+if not os.path.exists(testOutput + "SuccessfulResults" + timeStart + ".txt"):
+    f = open(testOutput + "SuccessfulResults" + timeStart + ".txt", "w")
+    f.write(platform.system() + " version: " + platform.version() + "  " + platform.machine() + "\n")
+    if os.path.isfile("../../../CompuCell3D-64bit website.url"):
+        f.write("CompuCell3D-64bit\n")
+    elif os.path.isfile("../../../CompuCell3D-32bit website.url"):
+        f.write("CompuCell3D-32bit\n")
+    f.close()
 
 #creates a place to store used demos so that multiple instances of this program be ran
 #also gets the initially used time if its been created already
-if(not os.path.isfile(testOutput + "ProcessedDemos" + timeStart + ".txt")):
-    file = open(testOutput + "ProcessedDemos" + timeStart + ".txt", "w")
-    file.write(timeStart + "\n")
-    file.close()
+if not os.path.isfile(testOutput + "ProcessedDemos" + timeStart + ".txt"):
+    f = open(testOutput + "ProcessedDemos" + timeStart + ".txt", "w")
+    f.write(timeStart + "\n")
+    f.close()
 else:
-    file = open(testOutput +" ProcessedDemos" + timeStart + ".txt", "r")
+    f = open(testOutput +" ProcessedDemos" + timeStart + ".txt", "r")
     timeStart = file.readline()[0:-1]
-    file.close()
+    f.close()
 
-#Collects all .cc3d files                                      
-demos = [os.path.join(dp, file) for dp, dn, filenames in os.walk(inputDirectory) for file in filenames if os.path.splitext(file)[1] == ".cc3d"]
-
-#writes the used to demos to a file
-def storeProcessedDemos(demoPath):
-    with open(testOutput + "ProcessedDemos" + timeStart + ".txt", 'a') as processedDemos:
-        processedDemos.writelines(demoPath + "\n")
-    processedDemos.close()
-
-def notInProcessedDemos(demoPath):
-    with open(testOutput + "ProcessedDemos" + timeStart + ".txt", 'r') as processedDemos:
-        for line in processedDemos:
-            if demoPath + "\n" == line:
-                return False
-        return True
-
+#Collects all .cc3d files
+demos = [os.path.join(dp, file) for dp, dn, fileNames in os.walk(inputDirectory) for file in fileNames if os.path.splitext(file)[1] == ".cc3d"]
 
 #Check for OS
 if platform.system() == "Linux":
     for path in demos:
         demoNumber += 1
         #Gets filename
-        if(notInProcessedDemos(path)):
+        if notInProcessedDemos(path):
             storeProcessedDemos(path)
             head, tail = os.path.split(path)
             ext = ".sh"
@@ -267,19 +300,22 @@ if platform.system() == "Linux":
             createCommand()
 elif platform.system() == "Windows":
     for path in demos:
+        print("Running " + path)
+        print("Don't kill the process!")
         demoNumber += 1
         #Gets filename
-        if(notInProcessedDemos(path)):
+        if notInProcessedDemos(path):
             storeProcessedDemos(path)
             head, tail = os.path.split(path)
             ext = ".bat"
             setup = os.getcwd() + "/"
             createCommand()
+        print("Finished")
 elif platform.system() == "Darwin":
     for path in demos:
         demoNumber += 1
         #Gets filename
-        if(notInProcessedDemos(path)):
+        if notInProcessedDemos(path):
             storeProcessedDemos(path)
             head, tail = os.path.split(path)
             ext = ".command"
